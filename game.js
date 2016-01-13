3e4e33118a31c8f4afb8d0b821826f50adfae432
Daniel Li <Danielxli3@gmail.com>
2016-01-12 21:34:53 -0800

    some changes

--- game.js
+++ game.js
@@ -1,27 +1,34 @@
-var questions = ["Do I like snacks?", "Do I wear contacts?", "Do I have any pets?", "How old am I?", "For the last challenge... Guess the number I am thinking... Between 1 and 100"]
-var answers = ["yes", "yes", "yes", 25, Math.floor((Math.random() * 100) + 1)]
+var ans1 = document.getElementById('a1');
+var ans2 = document.getElementById('a2');
+var ans3 = document.getElementById('a3');
+var ans4 = document.getElementById('a4');
+var ans5 = document.getElementById('a5');
 
 
-var counter = 0
+var questions = ["Do I like snacks?", "Do I wear contacts?", "Do I have any pets?", "How old am I?", "For the last challenge... Guess the number I am thinking... Between 1 and 100"];
+var answers = ["yes", "yes", "yes", 25, Math.floor((Math.random() * 100) + 1)];
 
-var userName = prompt("Hello and welcome to my game!\n What\'s your name?");
-alert("Hi, " + userName + ".\n Answer these questions about me as best you can!!");
 
+var counter = 0;
+
+var userName = prompt("Hey!!!! What\'s your name?");
+var welcomeMessage = document.getElementById('welcome');
+welcomeMessage.textContent = "Hello and welcome to the guessing game, " + userName + "!";
 
 function game(question, answer) {
   var ques = prompt(question).toLowerCase();
 
   if (answer === ques) {
     counter++;
-    alert("That is correct")
+    alert("That is correct");
   } else {
-    alert("That is incorrect")
+    alert("That is incorrect");
   }
 }
 
 for (var i = 0; i < questions.length; i++ ) {
-  console.log("Here's the answer ;)     " + answers[i])
-  game(questions[i], answers[i])
+  console.log("Here's the answer to question " + (i+1)  + ": " + answers[i]);
+  game(questions[i], answers[i]);
 
 }
 
