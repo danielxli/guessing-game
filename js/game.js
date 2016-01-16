3eeb55b99db3c40c13913ae9dd93cde9eb18c310
Daniel Li <Danielxli3@gmail.com>
2016-01-16 01:26:07 -0800

    lots of new stuff like getting rid of prompts and new images

--- js/game.js
+++ js/game.js
@@ -6,15 +6,15 @@ var ans5 = document.getElementById('a5');
 var ans6 = document.getElementById('a6');
 var results = [ans1, ans2, ans3, ans4, ans5, ans6];
 
-var introPic = document.getElementById("lobster");
+var introPic = document.getElementById('lobster');
 
 var questions = [
-  "Do I like snacks?",
-  "Do I wear contacts?",
-  "Do I have any pets?",
-  "How old am I?",
-  "For the last challenge... Guess the number I am thinking... Between 1 and 100",
-  "What are my favourite colours?"
+  'Do I like snacks?',
+  'Do I wear contacts?',
+  'Do I have any pets?',
+  'How old am I?',
+  'For the last challenge... Guess the number I am thinking... Between 1 and 100',
+  'What are my favourite colours?'
 ];
 
 var answers = [
@@ -27,31 +27,61 @@ var answers = [
 ];
 
 var submissions = [];
-var counter = 0;
+var scoreCounter = 0;
+var questionCounter = 0;
 
-var userName = prompt("Hey!!!! What\'s your name?");
-var welcomeMessage = document.getElementById('welcome');
-welcomeMessage.textContent = "Hello and welcome to the guessing game, " + userName + "!";
 
 var statusMessage = document.getElementById('status');
-statusMessage.textContent = "Get ready for your first question...";
+statusMessage.textContent = "Hello and welcome to the guessing game! Type your name below:";
 
-function game(question, answer) {
-  var ques = prompt(question).toLowerCase();
-  submissions.push(ques)
+function submitName(){
+  var userName = document.getElementById('inputBox').value
+  statusMessage.textContent = "Ok, " + userName + ". Click start game for your first question...";
+  document.getElementById('submitButton').setAttribute("onClick", "javascript: displayQuestion();" );
+  document.getElementById('inputBox').style.display='none';
+  document.getElementById('inputBox').value = ''
+  document.getElementById('submitButton').innerHTML = "Start game";
+}
 
-  for (var i = 0 ;i < answer.length; i++) {
-    if (i > 0) {
-      welcomeMessage.textContent = "";
-    }
-    if (answer[i] === ques || answer[i] === parseInt(ques)) {
-      introPic.src = "imgs/happypuppy.jpg";
-      counter++;
-      statusMessage.textContent = "Nice! That is correct \n Your score is : " + counter;
-      i = answer.length
-    } else if (i === answer.length - 1 ){
-      introPic.src = "imgs/sadpuppy.jpg";
-      statusMessage.textContent = "Aww... That is incorrect \n Your score is : " + counter;
+function displayQuestion(){
+  introPic.src = "imgs/waiting.gif";
+  statusMessage.textContent = questions[questionCounter];
+  document.getElementById('inputBox').style.display='inline';
+  document.getElementById('submitButton').setAttribute("onClick", "javascript: submitAnswer();" );
+  document.getElementById('submitButton').innerHTML = "Submit";
+}
+
+function submitAnswer(){
+  console.log("submitted");
+  var submission = document.getElementById('inputBox').value;
+  submissions.push(submission);
+
+  if (checker(submission, answers[questionCounter])) {
+    introPic.src = "imgs/happypuppy.jpg";
+    scoreCounter++;
+    questionCounter++
+    results[questionCounter].textContent = display(questionCounter,questions[questionCounter],answers[questionCounter],submissions[questionCounter-1])
+    statusMessage.textContent = "Nice! That is correct!  Your score is: " + scoreCounter + "/" + questionCounter;
+  } else {
+    introPic.src = "imgs/sadpuppy.jpg";
+    questionCounter++
+    results[questionCounter].textContent = display(questionCounter,questions[questionCounter],answers[questionCounter],submissions[questionCounter-1])
+    statusMessage.textContent = "Aww... That is incorrect... Your score is: " + scoreCounter + "/" + questionCounter;
+  }
+
+  document.getElementById('inputBox').value = ''
+  document.getElementById('inputBox').style.display='none';
+  document.getElementById('submitButton').setAttribute("onClick", "javascript: displayQuestion();" );
+  document.getElementById('submitButton').innerHTML = "Next question";
+}
+
+function checker(submission, answerArray) {
+  submission = submission.toLowerCase()
+  for (var i = 0; i < answerArray.length; i++) {
+    if (submission === answerArray[i] || parseInt(submission) === answerArray[i]) {
+      return true;
+    } else if (i === answerArray.length - 1 ){
+      return false;
     }
   }
 }
@@ -59,10 +89,4 @@ function game(question, answer) {
 function display (questionNumber,questionAsked,answerArray,submissionReceived) {
   var show = "Question " + questionNumber + " was '" + questionAsked + "' Acceptable answers were: '" + answerArray + ".' You said the answer was '" + submissionReceived + ".'"
   return show
-}
-
-for (var i = 0; i < questions.length; i++ ) {
-  console.log("Here's the answer to question " + (i+1)  + ": " + answers[i]);
-  game(questions[i], answers[i]);
-  results[i].textContent = display(i+1,questions[i],answers[i],submissions[i])
 }
